3e2021d44d887ba073cff7d041b98dac7685f6fb
Kumud <kumudkumar2001@gmail.com>
2023-10-31 21:51:26 +0530

    Final Commit

--- src/main/java/com/employeetax/api/entity/Employee.java
+++ src/main/java/com/employeetax/api/entity/Employee.java
@@ -3,7 +3,6 @@ package com.employeetax.api.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 import javax.validation.constraints.*;
 import java.util.List;
@@ -18,24 +17,29 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long employeeID;
 
-    @NotBlank
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "First name should contain only letters")
     private String firstName;
 
-    @NotBlank
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Last name should contain only letters")
     private String lastName;
 
-    @NotBlank
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
 
     @ElementCollection
     @CollectionTable(name = "phone_numbers", joinColumns = @JoinColumn(name = "employee_id"))
     @Column(name = "phone_number")
-    private List<String> phoneNumbers;
+    private List<@Pattern(regexp = "^[0-9]{10}$", message = "Phone number should contain exactly 10 digits") String> phoneNumbers;
 
-    @NotBlank
+    @NotBlank(message = "Date of Joining is required")
     private String doj;
 
-    @NotNull
+    @NotNull(message = "Salary is required")
+    @DecimalMin(value = "0.0", message = "Salary must be a positive number")
     private double salary;
-
 }
